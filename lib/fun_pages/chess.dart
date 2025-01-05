c9a311181597d9f70b427367cbd443c9f17609db
Andrew Kula <arkula931@gmail.com>
2025-01-05 16:14:19 -0600

    added chess board

--- lib/fun_pages/chess.dart
+++ lib/fun_pages/chess.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:squares/squares.dart';
 
 class Chess extends StatefulWidget {
   const Chess({super.key});
@@ -8,8 +9,83 @@ class Chess extends StatefulWidget {
 }
 
 class _ChessState extends State<Chess> {
+  BoardState state = const BoardState(board: [
+    'r',
+    'n',
+    'b',
+    'q',
+    'k',
+    'b',
+    'n',
+    'r',
+    'p',
+    'p',
+    'p',
+    'p',
+    'p',
+    'p',
+    'p',
+    'p',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    'P',
+    'P',
+    'P',
+    'P',
+    'P',
+    'P',
+    'P',
+    'P',
+    'R',
+    'N',
+    'B',
+    'Q',
+    'K',
+    'B',
+    'N',
+    'R'
+  ], turn: 0);
+
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello World');
+    return Scaffold(
+      appBar: AppBar(title: const Text('Chess')),
+      body: Center(
+        child: Board(
+          state: state,
+          pieceSet: PieceSet.merida(),
+        ),
+      ),
+    );
   }
 }
