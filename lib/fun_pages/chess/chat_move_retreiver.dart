87dbd0644f4927f4255b65ef0e69f266e8c9061e
Andrew Kula <arkula931@gmail.com>
2025-01-13 09:34:18 -0600

    bug fix

--- lib/fun_pages/chess/chat_move_retreiver.dart
+++ lib/fun_pages/chess/chat_move_retreiver.dart
@@ -2,18 +2,22 @@ import 'package:google_generative_ai/google_generative_ai.dart';
 import 'chess_env.dart';
 
 class GeminiMove {
-  final env = EnvVariables();
   Future<String> sendToGemini(String prompt) async {
-    final apiKey = env.getGeminiAPI();
+    try {
+      final env = EnvVariables();
+      final apiKey = env.getGeminiAPI();
 
-    final model = GenerativeModel(
-      model: 'gemini-1.5-flash-latest',
-      apiKey: apiKey,
-    );
+      final model = GenerativeModel(
+        model: 'gemini-1.5-flash-latest',
+        apiKey: apiKey,
+      );
 
-    final content = [Content.text(prompt)];
-    final response = await model.generateContent(content);
+      final content = [Content.text(prompt)];
+      final response = await model.generateContent(content);
 
-    return response.text ?? 'No adequate response';
+      return response.text ?? 'No adequate response';
+    } catch (e) {
+      return 'No adequate response (Web)';
+    }
   }
 }
