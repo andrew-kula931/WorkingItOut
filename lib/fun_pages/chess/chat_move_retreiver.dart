001b29d030877eb356e20806af9559384625c5f8
Andrew Kula <arkula931@gmail.com>
2025-01-13 09:43:57 -0600

    bug fix

--- lib/fun_pages/chess/chat_move_retreiver.dart
+++ lib/fun_pages/chess/chat_move_retreiver.dart
@@ -1,5 +1,5 @@
 import 'package:google_generative_ai/google_generative_ai.dart';
-import 'chess_env.dart';
+import 'chess_env.dart' if (dart.library.html) 'package:http/http.dart';
 
 class GeminiMove {
   Future<String> sendToGemini(String prompt) async {
